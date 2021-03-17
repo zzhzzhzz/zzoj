6bbb4c3375a3624e675b68bb2f9abbf99f389d47
zzhzzhzz <52144158+zzhzzhzz@users.noreply.github.com>
2021-03-17 21:19:35 +0800

    Create MyWebMvcConfigurer.java

---
+++ src/main/java/com/zzoj/app/zzoj/config/MyWebMvcConfigurer.java
@@ -0,0 +1,34 @@
+package com.zzoj.app.zzoj.config;
+
+import com.zzoj.app.zzoj.web.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.async.TimeoutCallableProcessingInterceptor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//MVC配置
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+
+    //拦截器配置
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new LoginInterceptor()).excludePathPatterns("/static/**","/login","/api/auLogin","/api/getRegYan","/api/reg","/reg");
+
+    }
+
+    @Override
+    public void configureAsyncSupport(final AsyncSupportConfigurer configurer) {
+        configurer.setDefaultTimeout(20000);
+        configurer.registerCallableInterceptors(timeoutInterceptor());
+    }
+    @Bean
+    public TimeoutCallableProcessingInterceptor timeoutInterceptor() {
+        return new TimeoutCallableProcessingInterceptor();
+    }
+
+
+}

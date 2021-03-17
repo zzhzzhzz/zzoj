28070de3e2da091054903897b17dbf290e593882
zzhzzhzz <52144158+zzhzzhzz@users.noreply.github.com>
2021-03-17 21:19:58 +0800

    Create RecordDao.java

---
+++ src/main/java/com/zzoj/app/zzoj/dao/RecordDao.java
@@ -0,0 +1,21 @@
+package com.zzoj.app.zzoj.dao;
+
+import com.zzoj.app.zzoj.domain.Record;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RecordDao {
+
+    void saveRecord(Record record);
+
+    //根据用户id查询记录总数
+    int getRecordCountByUserId(int id);
+
+    int getQuestionRecordCount(String qid);
+
+    List<Record> getRecordByUserId(int id, int start, int size);
+
+    int getBeatQuestionRecordCount(String que_id, int runtime);
+}
